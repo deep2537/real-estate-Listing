f87b00d083f34074e4bf29fc0e6406f171929cbf
Deep Gada <deepgada2003@gmail.com>
2024-12-27 16:53:28 +0530

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,48 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useRef } from "react";
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef =useRef(null);
+  const [file,setFile]=useState(undefined);
+  const [formData,setFormData]=useState({});
+ 
+  useEffect(()=>{
+    if(file){
+      handleFileUpload(file);
+    }
+  },[file]);
+  const handleFileUpload=async(file)=>{
+    const data =new FormData();
+    data.append("file",file);
+    data.append("upload_preset","real_estate_work");
+    data.append("cloud_name","dwjtkdwul");
+    const res=await fetch("https://api.cloudinary.com/v1_1/dwjtkdwul/image/upload",{
+      method:"POST",
+      body:data
+    })
+    const uploadedimageURL=await res.json();
+    console.log(uploadedimageURL.url);
+    if(uploadedimageURL){
+      setFormData({...formData,avatar:uploadedimageURL});
+      console.log(formData);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-3">
-        <img
-          src={currentUser.avatar}
+        <input type="file" className="" ref={fileRef} hidden accept="image/*" onChange={(e)=>setFile(e.target.files[0])}/>
+        <img onClick={()=>fileRef.current.click()}
+          src={formData.avatar.secure_url||currentUser.avatar}
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
         />
+        <p>
+          {
+
+          }
+        </p>
         <input
           type="text"
           placeholder="username"
