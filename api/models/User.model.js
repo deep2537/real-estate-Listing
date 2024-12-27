8bb7601381635cb746b62de0acf0dc3aeff95b66
Deep Gada <deepgada2003@gmail.com>
2024-12-27 13:24:33 +0530

    complete profile page UI

--- api/models/User.model.js
+++ api/models/User.model.js
@@ -15,7 +15,7 @@ const userSchema= new mongoose.Schema({
     },
     avatar:{
         type:String,
-        default:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Fuser_149071&psig=AOvVaw3SwpRLRHGebg5i1-cG76gV&ust=1735309927749000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMj3zLDTxYoDFQAAAAAdAAAAABAE",
+        default:"https://drive.google.com/file/d/1QYNXyTk0hayuCXaXkb0CHbJx6DXS_DLu/view",
     },
 },{timestamps:true});
 const User = mongoose.model("User",userSchema);
