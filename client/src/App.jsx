82cc13bc345890842c55a21942e17ba375e40099
Deep Gada <deepgada2003@gmail.com>
2024-12-28 19:34:17 +0530

    complete create listing page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import { BrowserRouter,Route,Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import SignIn from './pages/Signin'
-import SignUp from './pages/SignUp'
-import About from './pages/About'
-import Profile from './pages/Profile'
-import Header from './components/Header'
-import PrivateRoute from './components/PrivateRoute'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/createlisting";
 const App = () => {
   return (
-   <BrowserRouter>
+    <BrowserRouter>
       <Header></Header>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/signin' element={<SignIn/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route element={<PrivateRoute/>} >
-        <Route path='/profile' element={<Profile/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/createlisting" element={<CreateListing />} />
         </Route>
       </Routes>
-   </BrowserRouter>
-  )
-}
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { useRef } from "react";
 import { deleteUserStart, deleteUserSuccess, deleteUserfailure, logoutUserStart, logoutUserSuccess, logoutUserfailure, updateUserStart,updateUserSucess,updateUserfailure } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+import {Link} from "react-router-dom";
 const Profile = () => {
   const { currentUser ,loading,error} = useSelector((state) => state.user);
   const fileRef =useRef(null);
@@ -122,6 +123,9 @@ const Profile = () => {
         <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 hover:opacity-90 disabled:opacity-70 font-semibold">
           {loading?"Loading ...":"Update"}
         </button>
+      <Link  className="bg-green-700 text-white p-3 rounded-lg text-center font-semibold hover:opacity-90"to={'/createlisting'}>
+        Create Listing
+      </Link>
       </form>
       <div className="flex justify-between mt-4">
         <span onClick={handleDeleteUser}className="text-red-700 cursor-pointer">Delete Account</span>
