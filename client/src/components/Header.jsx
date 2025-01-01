c90f8be384925efce2f10c780636509ba8a61e1a
Deep Gada <deepgada2003@gmail.com>
2025-01-01 13:59:33 +0530

    complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ErrorBoundary from "./ErrorBoundary";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link ,useLocation,useNavigate} from "react-router-dom";
 import {useSelector} from 'react-redux';
 const Header = () => {
 const {currentUser}=useSelector(state=>state.user);
-
+const[searchTerm,SetSearchTerm]=useState('');
+const navigate=useNavigate();
+const handleSubmit=(e)=>{
+  e.preventDefault();
+  const urlParams=new URLSearchParams(window.location.search);
+  urlParams.set('search',searchTerm);
+  const searchQuery=urlParams.toString();
+  navigate(`/search?${searchQuery}`);
+}
+useEffect(()=>{
+  const urlParams=new URLSearchParams(location.search);
+  const searchtermFromUrl=urlParams.get('search');
+  if(searchtermFromUrl){
+    SetSearchTerm(searchtermFromUrl);
+    console.log(searchTerm);
+  }
+},[location.search]);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -16,13 +32,16 @@ const {currentUser}=useSelector(state=>state.user);
         </h1>
         </Link>
         <ErrorBoundary>
-          <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
             <input
               type="text"
               placeholder="Search..."
               className="bg-transparent focus:outline-none w-24 sm:w-64"
+              value={searchTerm}
+              onChange={(e)=>SetSearchTerm(e.target.value)}
             />
-            <FaSearch className="text-slate-600" />
+            <button>            <FaSearch className="text-slate-600" />
+            </button>
           </form>
         </ErrorBoundary>
         <ul className="flex gap-4">
